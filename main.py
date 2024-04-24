a58782ed475bb688edde75fd1aadaf0272fc3159
Faiaz <48489726+Faiaz3k@users.noreply.github.com>
2024-04-24 20:25:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import subprocess
 
+def run_module():
+    try:
+        # Change directory to the "modiule" directory
+        subprocess.run("cd modiule", shell=True, check=True)
+        
+        # Run the module
+        subprocess.run("python eyeboard.py", shell=True, check=True)
+        
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    run_module()
